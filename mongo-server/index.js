971cd2f6859fabf814d739b7d291ef826aba1fe6
saddamhr <saddambubt65@gmail.com>
2020-04-01 21:01:42 +0600

    node mongo

---
+++ mongo-server/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const uri = process.env.DB_PATH;
+
+let client = new MongoClient(uri, { useNewUrlParser: true });
+const users = ['Asad', 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana'];
+
+// DATABASE CONNECTION
+
+
+
+
+
+app.get('/products', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        // perform actions on the collection object
+        collection.find({name:'Mobile'}).limit(10).toArray((err, documents) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send({message:err})
+            } else {
+
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query);
+    const name = users[id];
+    res.send({ id, name });
+})
+
+// POST
+app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        // perform actions on the collection object
+        collection.insertOne(product, (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send({message:err})
+            } else {
+
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
+});
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => console.log('Listening to port 4200'));
